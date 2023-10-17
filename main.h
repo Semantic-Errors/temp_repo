151b93d31e316f54d16e8127f4bcc284344e76f2
mahmoudmuhammedd <138724393+mahmoudmuhammedd@users.noreply.github.com>
2023-10-17 04:22:03 +0300

    Update main.h

--- main.h
+++ main.h
@@ -10,7 +10,22 @@ typedef struct formats
 	int (*function)();
 } format_specifiers;
 
+/**
+ * struct flags - struct containing plus hash space flags
+ * it's turned on when a flag specifier is passed to _printf()
+ * @plus: flag for the "+" character
+ * @space: flag for the " " character
+ * @hash: flag for the "#" character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} diff_flags;
+/* putchar */
 int _putchar(int c);
+/* new printf */
 int _printf(const char *format, ...);
 void handle_specifier(const char *format, int i, int *printed_chars,
 			format_specifiers *formats_spec, va_list list);
@@ -18,6 +33,9 @@ void handle_specifier(const char *format, int i, int *printed_chars,
 int print_char(va_list list);
 int print_string(va_list list);
 int print_integer(va_list list);
+/* int to binary */
 int print_int_tobinary(va_list list);
+/* get flag */
+int get_flag_p_s_h(char spec, flags *f);
 
 #endif /* PRINTF_MAIN_H_ */
